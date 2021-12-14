f1693e901b13be4cc56fa58c24fecc6859b32f5d
2heeesss <hohooodo@gmail.com>
2021-12-14 19:51:43 +0900

    #13 Feat: add fetch api

--- public/js/store/scheduler.js
+++ public/js/store/scheduler.js
@@ -1,3 +1,5 @@
+import fetches from '../utils/fetch.js';
+
 const store = {
   _state: {
     schedules: [],
@@ -12,4 +14,43 @@ const store = {
   },
 };
 
-export default store;
+const fetchSchedules = async () => {
+  try {
+    store.schedules = await fetches.get('/schedules');
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+const addSchedules = async schedule => {
+  try {
+    const newSchedule = await fetches.post('/schedules', schedule);
+    store.schedules = [...store.schedules, newSchedule];
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+const updateSchedules = async (id, schedule) => {
+  try {
+    store.schedules = await fetches.patch(`/schedules/${id}`, schedule);
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+const removeSchedules = async id => {
+  try {
+    store.schedules = await fetches.delete(`/schedules/${id}`);
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+export default {
+  store,
+  fetchSchedules,
+  addSchedules,
+  updateSchedules,
+  removeSchedules,
+};
